9d9e91ee3cd413285d3aca198a5518d622a29b44
FOS-Intern <elbertchailes888@gmail.com>
2024-07-27 00:38:58 +0700

    axios template

---
+++ src/services/userService.ts
@@ -0,0 +1,9 @@
+// // src/services/userService.ts
+// import api from '../api/axios';
+// import { User } from '../types/User';
+
+// // Function to fetch users
+// export const fetchUsers = async (): Promise<User[]> => {
+//     const response = await api.get<User[]>('/users');
+//     return response.data;
+// };
---
+++ src/types/User.ts
@@ -0,0 +1,7 @@
+// // src/types/User.ts
+// export interface User {
+//     id: number;
+//     name: string;
+//     username: string;
+//     email: string;
+// }
