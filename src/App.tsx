eb9e98c45151d6b8a2d8074a2a413d3cdd3fcdee
FOS-Intern <elbertchailes888@gmail.com>
2024-07-26 22:36:55 +0700

    init fe prog

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+
+function App() {
+  return (
+    <>
+      <Label className="text-green-400">Hello again !</Label>
+      <Button>Hello World !</Button>
+    </>
+  );
+}
+
+export default App;
